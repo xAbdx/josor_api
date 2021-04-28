29b25550593eb50df43233b25a09f1042cf74d5a
xAbdx <67425264+xAbdx@users.noreply.github.com>
2021-04-28 04:02:42 +0300

    added changes to skills.php

---
+++ dashboard-login.php
@@ -0,0 +1,39 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json');
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "ourproject";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $params = (array) json_decode(file_get_contents('php://input'), TRUE);
+    $username = $params["UserName"];
+    $password = $params["Password"];
+    $userTypeid = $params["userTypeid"];
+    $sql = "select * from users where name='$username' and password='$password' and userTypeid='1'";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        if (mysqli_num_rows($result) == 1) {
+            $returnResponse = (object) [
+                "isAdmin" => true,
+                "errorMessage" => ""
+            ];
+        } else {
+            $returnResponse = (object) [
+                "isAdmin" => false,
+                "errorMessage" => "Invalid user type"
+            ];
+        }
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+echo json_encode($returnResponse);
--- skills.php
+++ skills.php
@@ -1,5 +1,7 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Headers: Content-Type');
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -9,19 +11,35 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
-}
-$sql = "SELECT * from skills";
-$result = mysqli_query($conn, $sql);
-$arr = array();
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while ($row = mysqli_fetch_assoc($result)) {
-        array_push($arr, $row);
-        // echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["password"]. "<br>". $row["email"]. "<br>";
+};
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $params = (array) json_decode(file_get_contents('php://input'), TRUE);
+    $id = $params["id"];
+    $name = $params["name"];
+
+    $sql = "INSERT INTO `skills` (`id`, `name`) VALUES ('$id', '$name');";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
 } else {
-    echo "0 results";
-}
-header('Content-Type: application/json');
-echo json_encode($arr);
+    $sql = "SELECT * from skills";
+    $result = mysqli_query($conn, $sql);
+    $arr = array();
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            array_push($arr, $row);
+            // echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["password"]. "<br>". $row["email"]. "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+    header('Content-Type: application/json');
+    echo json_encode($arr);
+};
+
 mysqli_close($conn);
